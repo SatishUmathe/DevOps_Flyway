fec7e1f148a61496978af33235950c996bf4caf0
sumathe_mmsrx <sumathe@primerx.io>
2025-10-29 20:32:55 +0530

    Updated

--- DevOps_FlyWay/Program.cs
+++ DevOps_FlyWay/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
+//using Microsoft.Data.SqlClient;
 
 namespace DevOps_FlyWay
 {
